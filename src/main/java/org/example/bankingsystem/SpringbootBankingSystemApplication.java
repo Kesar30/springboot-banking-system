2fed7242fc845789882fa15d5b16047c38f4b4e9
Kesar30 <Kesar4465@gmail.com>
2026-07-31 16:53:00 +0530

    Initial Commit

---
+++ src/main/java/org/example/bankingsystem/SpringbootBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBankingSystemApplication.class, args);
+    }
+
+}
