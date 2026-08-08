bddd75a73131837695905a1f74abaf8426806bd7
Kesar30 <Kesar4465@gmail.com>
2026-08-08 18:44:23 +0530

    implement custom exception

--- src/main/java/org/example/bankingsystem/controller/AccountController.java
+++ src/main/java/org/example/bankingsystem/controller/AccountController.java
@@ -1,6 +1,7 @@
 package org.example.bankingsystem.controller;
 
 import jakarta.validation.Valid;
+import org.example.bankingsystem.exceptions.AccountNotFoundException;
 import org.example.bankingsystem.requests.AccountRequest;
 import org.example.bankingsystem.services.AccountService;
 import org.example.bankingsystem.entities.Account;
@@ -18,51 +19,27 @@ public class AccountController {
     private AccountService accountService;
     @PostMapping("/accounts")
     public ResponseEntity<Account> create(@Valid @RequestBody AccountRequest request){
-        try {
             Account account =  accountService.createAccount(request);
             return ResponseEntity.status(HttpStatus.CREATED).body(account);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+
     }
 
     @GetMapping("/balance/{no}")
     public ResponseEntity<BigDecimal> balance(@PathVariable("no") long number){
-        try{
             BigDecimal balance = accountService.showBalance(number);
-            if (balance == null) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
+//            if (balance == null) {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//            }
             return ResponseEntity.ok(balance);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
     }
     @GetMapping("/accounts/user/{id}")
     public ResponseEntity<List<Account>> show(@PathVariable("id") int aid){
-        try{
             List<Account> accounts = accountService.showAccounts(aid);
-            if(accounts.isEmpty()){
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-            }
             return ResponseEntity.ok(accounts);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
     }
     @DeleteMapping("account/{no}")
     public ResponseEntity<Void> delete(@PathVariable("no") long accountNo){
-       try{
            accountService.deleteAccount(accountNo);
            return ResponseEntity.noContent().build();
-       } catch (Exception e) {
-           return ResponseEntity.internalServerError().build();
-       }
     }
-
-
 }
--- src/main/java/org/example/bankingsystem/services/AccountService.java
+++ src/main/java/org/example/bankingsystem/services/AccountService.java
@@ -2,11 +2,12 @@ package org.example.bankingsystem.services;
 
 import org.example.bankingsystem.entities.Account;
 import org.example.bankingsystem.entities.User;
+import org.example.bankingsystem.exceptions.AccountNotFoundException;
+import org.example.bankingsystem.exceptions.UserNotFoundException;
 import org.example.bankingsystem.repository.AccountRepository;
 import org.example.bankingsystem.repository.UserRepository;
 import org.example.bankingsystem.requests.AccountRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -20,11 +21,24 @@ public class AccountService {
     @Autowired
     private UserRepository userRepository;
     //method to create account number
-    public Account createAccount(AccountRequest request){
+    public Account createAccount(AccountRequest request) {
+
         Account account = new Account();
         account.setAccountType(request.getAccountType());
-//        account.setBalance(request.getBalance());
-        account.setUser(request.getUser());
+        User user = userRepository.findById(request.getUser().getUserId())
+                .orElseThrow(() ->
+                        new UserNotFoundException("User not found with Id : " + request.getUser().getUserId()));
+
+//        Optional<User> optionalUser =
+//                userRepository.findById(request.getUser().getUserId());
+//
+//        if (optionalUser.isEmpty()) {
+//            throw new UserNotFoundException(
+//                    "User not found with Id : " + request.getUser().getUserId());
+//        }
+//        User user = optionalUser.get();
+        account.setUser(user);
+
         return accountRepository.save(account);
     }
     //getting balance of the account
@@ -34,16 +48,24 @@ public class AccountService {
             Account a = account.get();
             return a.getBalance();
         }
-        return null;
+        throw new AccountNotFoundException("Account not found with account number : "+accNo);
     }
     //getting one or more account number of user
     public List<Account> showAccounts(Integer userId) {
         if (!userRepository.existsById(userId)) {
-            return null;
+            throw new UserNotFoundException("User not found with Id : "+userId);
         }
-        return accountRepository.findByUserUserId(userId);
+        List<Account> accounts = accountRepository.findByUserUserId(userId);
+        if (accounts.isEmpty()){
+            throw new AccountNotFoundException("No Accounts found for User id : "+userId);
+        }
+        return accounts;
     }
     public void deleteAccount(long accountNo){
+        if (!accountRepository.existsById(accountNo)) {
+            throw new AccountNotFoundException("Account not found with account number : "+accountNo);
+
+        }
         accountRepository.deleteById(accountNo);
     }
 
