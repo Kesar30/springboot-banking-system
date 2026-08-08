1805fca877c3cd163d7a45a03017a3ddbe589a27
Kesar30 <Kesar4465@gmail.com>
2026-08-08 17:39:40 +0530

    add global Exception handler

---
+++ src/main/java/org/example/bankingsystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package org.example.bankingsystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //validation exception
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleInvalidArguments(MethodArgumentNotValidException e){
+        Map<String,String> exceptions = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            exceptions.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return exceptions;
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public Map<String , String> handleUserNotFoundException(UserNotFoundException e){
+        Map<String,String> exceptions = new HashMap<>();
+        exceptions.put("error" , e.getMessage());
+        return exceptions;
+    }
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(InvalidPasswordException.class)
+    public Map<String , String> handleInvalidPasswordException(InvalidPasswordException e){
+        Map<String,String> exceptions = new HashMap<>();
+        exceptions.put("error" , e.getMessage());
+        return exceptions;
+    }
+}
