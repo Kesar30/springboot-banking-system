6c6faac1610bc0d3d1e35629189eef03707f94d5
Kesar30 <Kesar4465@gmail.com>
2026-08-08 17:25:34 +0530

    add User Controller

---
+++ src/main/java/org/example/bankingsystem/controller/UserController.java
@@ -0,0 +1,75 @@
+package org.example.bankingsystem.controller;
+
+import jakarta.validation.Valid;
+import org.example.bankingsystem.exceptions.UserNotFoundException;
+import org.example.bankingsystem.requests.LoginRequest;
+import org.example.bankingsystem.requests.UserRequest;
+import org.example.bankingsystem.services.UserService;
+import org.example.bankingsystem.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    //post method to add user
+    @PostMapping("/users")
+    public ResponseEntity<User> add(@Valid @RequestBody UserRequest request){
+        User u = null;
+        try {
+            u = userService.addUser(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(u);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    //get method to show user data by id
+    @PostMapping("/users/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest) {
+        User user = userService.loginUser(loginRequest);
+        return ResponseEntity.ok(user); // 200 OK with user data
+        //without exception Handling
+//        if (user != null) {
+//            if (user.getPassword().equals(loginRequest.getPassword())) {
+//                return ResponseEntity.ok(user); // 200 OK with user data
+//            }
+//            else {
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                        .body("Invalid password");
+//            }
+//        } else {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//                    .body("User not found");
+//        }
+    }
+    //update user detail
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> update(@Valid @RequestBody UserRequest request,@PathVariable("id") Integer uid ){
+        try{
+            User updatedUser = userService.updateUser(request, uid);
+            return  ResponseEntity.ok().body(updatedUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    //delete user data
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Integer uid){
+        try{
+            userService.deleteUser(uid);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+}
